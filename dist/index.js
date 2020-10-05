95fdbdc6ac1165762e849be608d024d78f7d17bb
Justin Leider <justin@leiders.org>
2020-10-05 21:49:00 +0000

    Add esMolduleInterop for webpack bundling

--- dist/index.js
+++ dist/index.js
@@ -35,9 +35,12 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.minimumBytes = exports.fileTypeExt = void 0;
-var file_type_1 = require("file-type");
+var file_type_1 = __importDefault(require("file-type"));
 exports.fileTypeExt = function (input) {
     if (!(input instanceof Uint8Array || input instanceof ArrayBuffer || Buffer.isBuffer(input))) {
         throw new TypeError("Expected the `input` argument to be of type `Uint8Array` or `Buffer` or `ArrayBuffer`, got `" + typeof input + "`");
